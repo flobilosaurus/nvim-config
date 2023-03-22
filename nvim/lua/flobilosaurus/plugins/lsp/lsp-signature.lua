5a589f8b1b67d6b829a698dceea3c9771d499b58
git-saver <>
2023-03-22 22:29:29 +0100

    updated by https://github.com/flobilosaurus/git-saver

--- nvim/lua/flobilosaurus/plugins/lsp/lsp-signature.lua
+++
@@ -1,6 +0,0 @@
-local lspsignature_setup, lspsignature = pcall(require, "lsp_signature")
-if not lspsignature_setup then
-	return
-end
-
-lspsignature.setup({})
--- nvim/lua/flobilosaurus/plugins/which-key.lua
+++
@@ -1,7 +0,0 @@
--- import nvim-treesitter plugin safely
-local status, whichkey = pcall(require, "which-key")
-if not status then
-	return
-end
-
-whichkey.setup({})
