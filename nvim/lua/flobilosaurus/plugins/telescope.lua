0f9632f97c17fb506501c4a05f9be620f1fcfa3f
git-saver <>
2023-05-19 14:40:34 +0200

    updated by https://github.com/flobilosaurus/git-saver

--- nvim/lua/flobilosaurus/plugins/telescope.lua
+++ nvim/lua/flobilosaurus/plugins/telescope.lua
@@ -25,51 +25,36 @@ return {
 				},
 				defaults = {
 					file_ignore_patterns = { "node_modules", ".git/" },
+					layout_config = {
+						horizontal = {
+							prompt_position = "bottom",
+							preview_width = 0.55,
+						},
+						width = 0.85,
+						height = 0.80,
+					},
 				},
 			})
-			local plenary = require("plenary.path")
 			local builtin = require("telescope.builtin")
-			local themes = require("telescope.themes")
 
-			local dropdown = themes.get_dropdown({
-				hidden = true,
-				no_ignore = true,
-				previewer = false,
-				prompt_title = "",
-				preview_title = "",
-				results_title = "",
-				layout_config = { prompt_position = "top" },
-			})
-
-			-- File browser always relative to buffer
-			local opts_file_browser = vim.tbl_extend("force", dropdown, {
-				path = "%:p:h",
-			})
-
-			-- Set current folder as prompt title
-			local with_title = function(opts, extra)
-				extra = extra or {}
-				local path = opts.cwd or opts.path or extra.cwd or extra.path or nil
-				local title = ""
-				local buf_path = vim.fn.expand("%:p:h")
-				local cwd = vim.fn.getcwd()
-				if path ~= nil and buf_path ~= cwd then
-					title = plenary:new(buf_path):make_relative(cwd)
-				else
-					title = vim.fn.fnamemodify(cwd, ":t")
-				end
-
-				return vim.tbl_extend("force", opts, {
-					prompt_title = title,
-				}, extra or {})
-			end
 			vim.api.nvim_create_autocmd("VimEnter", {
 				callback = function()
 					-- Open file browser if argument is a folder
 					local arg = vim.api.nvim_eval("argv(0)")
 					if arg and (vim.fn.isdirectory(arg) ~= 0 or arg == "") then
 						vim.defer_fn(function()
-							builtin.find_files(with_title(dropdown))
+							builtin.find_files({
+								previewer = false,
+								prompt_title = "",
+								preview_title = "",
+								results_title = "",
+								hidden = true,
+								no_ignore = true,
+								layout_config = {
+									width = 0.40,
+									height = 0.40,
+								},
+							})
 						end, 10)
 					end
 				end,
